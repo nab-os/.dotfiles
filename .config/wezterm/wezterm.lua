2b3c12a25dba9f450dca408e1e68cce41b50bf14
Nabos <nabos@glargh.fr>
2024-01-10 10:12:21 +0100

    Custom wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,49 +1,22 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
 local act = wezterm.action
 
--- This table will hold the configuration.
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
-config.font = wezterm.font_with_fallback({
-  { family = "Inconsolata",  scale = 1 },
-  { family = "Arial", scale = 1 },
-})
-config.warn_about_missing_glyphs = false
-config.color_scheme = 'Solarized (dark) (terminal.sexy)'
-config.window_background_opacity = 0.9
-config.window_decorations = "RESIZE"
 config.window_close_confirmation = "AlwaysPrompt"
 config.scrollback_lines = 40000
 config.default_workspace = "home"
 
--- Dim inactive panes
-config.inactive_pane_hsb = {
-  saturation = 0.7,
-  brightness = 0.5
-}
-
 -- Keys
--- config.leader = { key = "b", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
   { key = "=", mods = "ALT",       action = act.ActivateCopyMode },
-  -- { key = "c", mods = 'NONE', action = act.CopyMode 'MoveLeft' },
-  -- { key = "t", mods = 'NONE', action = act.CopyMode 'MoveDown' },
-  -- { key = "s", mods = 'NONE', action = act.CopyMode 'MoveUp' },
-  -- { key = "r", mods = 'NONE', action = act.CopyMode 'MoveRight' },
 
-  -- Pane keybindings
+  -- Panes
   { key = "-", mods = "ALT",       action = act.SplitVertical { domain = "CurrentPaneDomain" } },
-  -- SHIFT is for when caps lock is on
   { key = "/", mods = "ALT", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
   { key = "c", mods = "ALT",       action = act.ActivatePaneDirection("Left") },
   { key = "t", mods = "ALT",       action = act.ActivatePaneDirection("Down") },
@@ -53,16 +26,48 @@ config.keys = {
   { key = "f", mods = "ALT",       action = act.TogglePaneZoomState },
   { key = "l", mods = "ALT",    action = act.ActivateKeyTable { name = "resize_pane", one_shot = false } },
 
-  -- Tab keybindings
+  -- Tabs
   { key = "n", mods = "ALT",       action = act.SpawnTab("CurrentPaneDomain") },
   { key = "c", mods = "SHIFT|ALT",       action = act.ActivateTabRelative(-1) },
   { key = "c", mods = "CTRL|ALT",       action = act.ActivateTabRelative(-1) },
   { key = "r", mods = "SHIFT|ALT",       action = act.ActivateTabRelative(1) },
   { key = "r", mods = "CTRL|ALT",       action = act.ActivateTabRelative(1) },
   { key = "m", mods = "ALT",       action = act.ActivateKeyTable { name = "move_tab", one_shot = false } },
+  { key = "'",
+    mods = "ALT",
+    action = act.PromptInputLine {
+      description = 'Enter new name for tab',
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  },
 
   -- Workspaces
   { key = "w", mods = "ALT",       action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
+  {
+    key = 'w',
+    mods = 'CTRL|ALT',
+    action = act.PromptInputLine {
+      description = 'Enter name for new workspace',
+      action = wezterm.action_callback(function(window, pane, line)
+        if line then
+          window:perform_action(
+            act.SwitchToWorkspace {
+              name = line,
+            },
+            pane
+          )
+        end
+      end),
+    },
+  },
+
+  -- Misc
+  { key = 'p', mods = 'ALT', action = act.ActivateCommandPalette },
+  { key = 'e', mods = 'ALT', action = act.CharSelect },
 }
 
 for i = 1, 9 do
@@ -153,66 +158,191 @@ config.key_tables = {
     { key = 'RightArrow', mods = 'ALT', action = act.CopyMode 'MoveForwardWord' },
     { key = 'UpArrow', mods = 'NONE', action = act.CopyMode 'MoveUp' },
     { key = 'DownArrow', mods = 'NONE', action = act.CopyMode 'MoveDown' },
-  }}  -- copy_mode = {
-  --   { key = 'n', mods = 'CTRL', action = act.CopyMode 'NextMatch' },
-  --   { key = 'p', mods = 'CTRL', action = act.CopyMode 'PriorMatch' },
-  --   { key = 'r', mods = 'CTRL', action = act.CopyMode 'CycleMatchType' },
-  --   { key = 'u', mods = 'CTRL', action = act.CopyMode 'ClearPattern' },
-  --   { key = 'PageUp', mods = 'NONE', action = act.CopyMode 'PriorMatchPage' },
-  --   { key = 'PageDown', mods = 'NONE', action = act.CopyMode 'NextMatchPage' },
-  --   { key = 'UpArrow', mods = 'NONE', action = act.CopyMode 'PriorMatch' },
-  --   { key = 'DownArrow', mods = 'NONE', action = act.CopyMode 'NextMatch' },
-  --   { key = 'Escape', mods = 'NONE', action = act.CopyMode 'Close' },
-  --   { key = 'Enter', mods = 'NONE', action = act.CopyMode 'PriorMatch' },
-  -- },}
+  }
+}
 
+-- Domains
+function file_exists(name)
+   local f=io.open(name,"r")
+   if f~=nil then io.close(f) return true else return false end
+end
 
-config.ssh_domains = {
-  {
-    name = 'glargh',
-    remote_address = 'glargh',
-    username = 'nabos',
-  },
+if file_exists(".config/wezterm/domains.lua") then
+  local domains = require 'domains'
+  config.ssh_domains = domains.ssh_domains
+  config.unix_domains = domains.unix_domains
+end
+  
+-- Theme
+config.font = wezterm.font_with_fallback({
+  { family = "Inconsolata",  scale = 1 },
+  { family = "Arial", scale = 1 },
+})
+config.warn_about_missing_glyphs = false
+config.window_background_opacity = 0.9
+config.window_decorations = "RESIZE"
+
+config.color_scheme = 'Solarized (dark) (terminal.sexy)'
+
+-- Dim inactive panes
+config.inactive_pane_hsb = {
+  saturation = 0.7,
+  brightness = 0.5
 }
 
 -- Tab bar
--- I don't like the look of "fancy" tab bar
 config.use_fancy_tab_bar = false
 config.status_update_interval = 1000
-wezterm.on("update-right-status", function(window, pane)
-  -- Workspace name
-  local stat = window:active_workspace()
-  -- It's a little silly to have workspace name all the time
-  -- Utilize this to display LDR or current key table name
-  if window:active_key_table() then stat = window:active_key_table() end
-  if window:leader_is_active() then stat = "LDR" end
+config.colors = {
+  tab_bar = {
+    background = '#002b36',
+    new_tab = {
+      bg_color = '#002b36',
+      fg_color = '#808080',
+    },
+  },
+}
 
-  -- Current working directory
-  local basename = function(s)
-    -- Nothign a little regex can't fix
-    return string.gsub(s, "(.*[/\\])(.*)", "%2")
+-- The filled in variant of the < symbol
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.pl_right_hard_divider
+
+-- The filled in variant of the > symbol
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.pl_left_hard_divider
+
+-- This function returns the suggested title for a tab.
+-- It prefers the title that was set via `tab:set_title()`
+-- or `wezterm cli set-tab-title`, but falls back to the
+-- title of the active pane in that tab.
+function tab_title(tab_info)
+  local title = tab_info.tab_title
+  -- if the tab title is explicitly set, take that
+  if title and #title > 0 then
+    return title
   end
-  local cwd = basename(pane:get_current_working_dir())
-  -- Current command
-  local cmd = basename(pane:get_foreground_process_name())
+  -- Otherwise, use the title from the active pane
+  -- in that tab
+  return tab_info.active_pane.title
+end
 
-  -- Time
-  local time = wezterm.strftime("%H:%M")
+wezterm.on(
+  'format-tab-title',
+  function(tab, tabs, panes, config, hover, max_width)
+    local edge_background = '#002b36'
+    local background = '#3c1361'
+    local foreground = '#808080'
 
-  -- Let's add color to one of the components
-  window:set_right_status(wezterm.format({
-    -- Wezterm has a built-in nerd fonts
-    { Text = wezterm.nerdfonts.oct_table .. "  " .. stat },
-    { Text = " | " },
-    { Text = wezterm.nerdfonts.md_folder .. "  " .. cwd },
-    { Text = " | " },
-    { Foreground = { Color = "FFB86C" } },
-    { Text = wezterm.nerdfonts.fa_code .. "  " .. cmd },
-    "ResetAttributes",
-    { Text = " | " },
-    { Text = wezterm.nerdfonts.md_clock .. "  " .. time },
-    { Text = " |" },
-  }))
+    if tab.is_active then
+      background = '#52307c'
+      foreground = '#c0c0c0'
+    elseif hover then
+      background = '#3b3052'
+      foreground = '#909090'
+    end
+
+    local edge_foreground = background
+
+    local title = tab_title(tab)
+
+    -- ensure that the titles fit in the available space,
+    -- and that we have room for the edges.
+    title = wezterm.truncate_right(title, max_width - 2)
+
+    return {
+      { Background = { Color = edge_background } },
+      { Foreground = { Color = edge_foreground } },
+      { Text = SOLID_LEFT_ARROW },
+      { Background = { Color = background } },
+      { Foreground = { Color = foreground } },
+      { Text = title },
+      { Background = { Color = edge_background } },
+      { Foreground = { Color = edge_foreground } },
+      { Text = SOLID_RIGHT_ARROW },
+    }
+  end
+)
+
+wezterm.on('update-right-status', function(window, pane)
+  -- Each element holds the text for a cell in a "powerline" style << fade
+  local cells = {}
+
+  -- Figure out the cwd and host of the current pane.
+  -- This will pick up the hostname for the remote host if your
+  -- shell is using OSC 7 on the remote host.
+  local cwd_uri = pane:get_current_working_dir()
+  if cwd_uri then
+    local cwd = ''
+    local hostname = ''
+
+    if type(cwd_uri) == 'userdata' then
+      -- Running on a newer version of wezterm and we have
+      -- a URL object here, making this simple!
+
+      cwd = cwd_uri.file_path
+      hostname = cwd_uri.host or wezterm.hostname()
+    else
+      -- an older version of wezterm, 20230712-072601-f4abf8fd or earlier,
+      -- which doesn't have the Url object
+      cwd_uri = cwd_uri:sub(8)
+      local slash = cwd_uri:find '/'
+      if slash then
+        hostname = cwd_uri:sub(1, slash - 1)
+        -- and extract the cwd from the uri, decoding %-encoding
+        cwd = cwd_uri:sub(slash):gsub('%%(%x%x)', function(hex)
+          return string.char(tonumber(hex, 16))
+        end)
+      end
+    end
+
+    -- Remove the domain name portion of the hostname
+    local dot = hostname:find '[.]'
+    if dot then
+      hostname = hostname:sub(1, dot - 1)
+    end
+    if hostname == '' then
+      hostname = wezterm.hostname()
+    end
+
+    table.insert(cells, cwd)
+    table.insert(cells, hostname)
+  end
+
+  -- The filled in variant of the < symbol
+  local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+
+  -- Color palette for the backgrounds of each cell
+  local colors = {
+    '#3c1361',
+    '#52307c',
+    '#663a82',
+    '#7c5295',
+    '#b491c8',
+  }
+
+  -- Foreground color for the text across the fade
+  local text_fg = '#c0c0c0'
+
+  -- The elements to be formatted
+  local elements = {}
+  -- How many cells have been formatted
+  local num_cells = 0
+
+  -- Translate a cell into elements
+  function push(text, is_last)
+    local cell_no = num_cells + 1
+    table.insert(elements, { Foreground = { Color = colors[cell_no] } })
+    table.insert(elements, { Text = SOLID_LEFT_ARROW })
+    table.insert(elements, { Foreground = { Color = text_fg } })
+    table.insert(elements, { Background = { Color = colors[cell_no] } })
+    table.insert(elements, { Text = ' ' .. text .. ' ' })
+    num_cells = num_cells + 1
+  end
+
+  while #cells > 0 do
+    local cell = table.remove(cells, 1)
+    push(cell, #cells == 0)
+  end
+
+  window:set_right_status(wezterm.format(elements))
 end)
 
 return config
